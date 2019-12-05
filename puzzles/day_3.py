2ca0cfb6d012c0b464f2fc86e6401455955b5cbf
derekvonk <derek.vonk@globalorange.nl>
2019-12-05 17:28:09 +0100

    WIP

--- puzzles/day_3.py
+++ puzzles/day_3.py
@@ -6,9 +6,48 @@ with open("../resources/input_day_3.txt") as file:
     wire_a = file.readline()
     wire_b = file.readline()
 
-# initialize grid
+# a line has:
+# start - int
+# end - int
+# direction - horizontal/vertical
+# height - int
 
-# function for navigating wires on grid
+# all lines belonging to wire a
+lines_a = []
+# all lines belonging to wire b
+lines_b = []
+# all lines that intersect
+crossing = []
 
-# function for determining intersections
+
+# function generating a line
+def gen_line(op: str, start: tuple):
+    if op[0] == 'R':
+        return [start, (start[0] + int(op[1:]), start[1]), 'hor', start[1]]
+    if op[0] == 'U':
+        return [start, (start[0], start[1] + int(op[1:])), 'ver', start[1] + int(op[1:])]
+    if op[0] == 'D':
+        return [start, (start[0], start[1] - int(op[1:])), 'ver', start[1] - int(op[1:])]
+    if op[0] == 'L':
+        return [start, (start[0] - int(op[1:]), start[1]), 'hor', start[1]]
+
+
+# loop over all wire directions and populate lines_a & lines_b
+start = (0, 0)
+for s in wire_a.split(','):
+    line = gen_line(s, start)
+    lines_a.append(line)
+    start = line[1]
+
+start = (0, 0)
+for s in wire_b.split(','):
+    line = gen_line(s, start)
+    lines_b.append(line)
+    start = line[1]
+
+
+# function for comparing line crossings, returning a list of line pairs that have intersections
+
+
+# function for calculating the manhattan distance of each intersection point and returning the lowest
 
